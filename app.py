8513710f87d271bd1746994f965655a23764bfe0
Soobin <nhsb00@hotmail.com>
2022-02-24 09:18:41 -0500

    Pythong, flask, Javascript, jQuery, Naver Map API, selenium, bs4, pymongo, requests, jinja2 are used to create yelp like webpage

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
+@app.route('/map')
+def test_map():
+    return render_template("prac_map.html")
+
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    title_receive = request.form["title_give"]
+    address_receive = request.form["address_give"]
+    action_receive = request.form["action_give"]
+    print(title_receive, address_receive, action_receive)
+
+    if action_receive == "like":
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$set": {"liked": True}})
+    else:
+        db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$unset": {"liked": False}})
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
